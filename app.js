54437838b5c40334093d93e5f9082ca2232df96e
Lozanciuc Sabina <sabinalozanciuc2000@gmail.com>
2020-07-20 15:06:41 +0300

    Firebase hosting, media queries, a.o. changes

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
+let viewportHeight = window.innerHeight;
+
+let hero = document.querySelector(".hero");
+let welcome = document.querySelector(".welcome");
+let contact = document.querySelector(".contact-form-wrapper");
+let footer = document.querySelector("footer");
 let categories = document.querySelectorAll(".photo-category");
+
+hero.style = `height: ${viewportHeight}px;`;
+welcome.style = `height: ${viewportHeight}px;`;
+contact.style = `height: ${viewportHeight}px;`;
+footer.style = `height: ${viewportHeight}px;`;
+
 let urls = [
   "img/gallery-photo1.jpg",
   "img/gallery-photo2.jpg",
   "img/gallery-photo3.jpg",
   "img/gallery-photo4.jpg",
 ];
-
 for (let i = 0; i < urls.length; i++) {
   categories[i].style = `
     background: linear-gradient( 0deg,
         rgba(31, 31, 31, 0.73),
         rgba(31, 31, 31, 0.73)
       ), url('${urls[i]}');
-  background-position: center;
-  background-size: cover;
-  background-repeat: no-repeat;`;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: ${viewportHeight / 2}px`;
 }
-
-let viewportHeight = window.innerHeight;
-let hero = document.querySelector(".hero");
-let heroHeightVH =
-  100 - (2 /* 2rem -> nav height*/ * 16 * 100) / viewportHeight;
-let heroHeightPX = viewportHeight - 2 /* 2rem -> nav height*/ * 16;
-hero.style = `height: ${viewportHeight}px;`;
-console.log(window.innerHeight);
