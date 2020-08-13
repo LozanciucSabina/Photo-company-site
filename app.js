a21942ab9d03313323420d2b873b3640defe4530
Lozanciuc Sabina <sabinalozanciuc2000@gmail.com>
2020-08-13 19:41:01 +0300

    Responsive features, mouse effects, form handling

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let categories = document.querySelectorAll(".photo-category");
 
 hero.style = `height: ${viewportHeight}px;`;
 welcome.style = `height: ${viewportHeight}px;`;
-contact.style = `height: ${viewportHeight}px;`;
+contact.style = `height: ${viewportHeight + 50}px;`;
 footer.style = `height: ${viewportHeight}px;`;
 
 let urls = [
@@ -17,6 +17,7 @@ let urls = [
   "img/gallery-photo3.jpg",
   "img/gallery-photo4.jpg",
 ];
+
 for (let i = 0; i < urls.length; i++) {
   categories[i].style = `
     background: linear-gradient( 0deg,
@@ -28,3 +29,42 @@ for (let i = 0; i < urls.length; i++) {
     background-repeat: no-repeat;
     height: ${viewportHeight / 2}px`;
 }
+
+let formButton = contact.querySelector("input[type = 'button']");
+
+class Customer {
+  constructor(name, surname, phone, message) {
+    this.name = name;
+    this.surname = surname;
+    this.phone = phone;
+    this.message = message;
+  }
+}
+
+formButton.addEventListener("click", () => {
+  let form = contact.querySelector("form");
+  let name = form.querySelector("#name").value;
+  let surname = form.querySelector("#surname").value;
+  let phone = form.querySelector("#phone").value;
+  let message = form.querySelector("#message").value;
+
+  let customer = new Customer(name, surname, phone, message);
+  console.log(customer);
+
+  form.reset();
+});
+
+categories.forEach((category) => {
+  let firstChild = category.firstElementChild;
+  let lastChild = category.lastElementChild;
+
+  category.onmouseover = () => {
+    firstChild.style.left = "1rem";
+    lastChild.style.right = "1rem";
+  };
+
+  category.onmouseout = () => {
+    firstChild.style.left = "0rem";
+    lastChild.style.right = "0rem";
+  };
+});
